343d313cec35e0b534ff1b4a514400f5ffc0783e
davi-byu <davivictor2016.2@gmail.com>
2026-09-12 23:40:12 -0300

    Adicionei um comentario

--- semana01/Exercicio1/Program.cs
+++ semana01/Exercicio1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        //pedir ao usuario pelo o seu primeiro nome
         Console.Write("Qual é o seu primeiro nome? ");
         string primeiroNome = Console.ReadLine();
 
