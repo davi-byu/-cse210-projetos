0f26280f1f54dc50d2724e81963d97322a30a031
davi-byu <davivictor2016.2@gmail.com>
2026-09-13 20:30:34 -0300

    Conclui Exercicio 3 jogo de adivinhacao

--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio3.");
+        Random geradorAleatorio = new Random();
+
+
+        int numeroMagico = geradorAleatorio.Next(1, 101);
+
+        int tentativas = 0;
+
+        Console.WriteLine("Qual é o seu palpite? ");
+        string palpiteDigitado = Console.ReadLine();
+
+        int palpite = int.Parse(palpiteDigitado);
+
+        tentativas++;
+
+        while (palpite != numeroMagico)
+        {
+            if (palpite < numeroMagico)
+            {
+                Console.WriteLine("Mais alto");
+            }
+
+            else if (palpite > numeroMagico)
+            {
+                Console.WriteLine("Mais baixo");
+            }
+
+            Console.WriteLine("Qual é o seu palpite? ");
+
+            palpiteDigitado = Console.ReadLine();
+            palpite = int.Parse(palpiteDigitado);
+            tentativas++;
+        }
+
+        Console.WriteLine("Você adivinhou! ");
+
+        Console.WriteLine($"Você fez {tentativas} palpites. ");
+
     }
 }
