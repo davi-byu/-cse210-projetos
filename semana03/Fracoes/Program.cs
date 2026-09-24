68c6b693f28c2772af8d1296a278f699e2eebfc0
davi-byu <davivictor2016.2@gmail.com>
2026-09-24 14:52:57 -0300

    Implementa classe Fracao

--- semana03/Fracoes/Program.cs
+++ semana03/Fracoes/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Fracoes.");
+        Fracao fracao1 = new Fracao();
+
+        Console.WriteLine(fracao1.ObterFracaoEmTexto());
+        Console.WriteLine(fracao1.ObterFracaoEmDecimal());
+
+        Fracao fracao2 = new Fracao(5);
+
+        Console.WriteLine(fracao2.ObterFracaoEmTexto());
+        Console.WriteLine(fracao2.ObterFracaoEmDecimal());
+
+        Fracao fracao3 = new Fracao(3, 4);
+
+        Console.WriteLine(fracao3.ObterFracaoEmTexto());
+        Console.WriteLine(fracao3.ObterFracaoEmDecimal());
+
+        Fracao fracao4 = new Fracao(1, 3);
+
+        Console.WriteLine(fracao4.ObterFracaoEmTexto());
+        Console.WriteLine(fracao4.ObterFracaoEmDecimal());
+
+        Fracao fracao5 = new Fracao();
+
+        fracao5.DefinirNumerador(7);
+        fracao5.DefinirDenominador(8);
+
+        Console.WriteLine(fracao5.ObterNumerador());
+        Console.WriteLine(fracao5.ObterDenominador());
     }
 }
